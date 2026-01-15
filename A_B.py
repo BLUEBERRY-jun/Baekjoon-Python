57a791f4cadfffb36fb574711380d43572c02590
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-15 12:12:47 +0900

    1000번, 1001번, 10998번, 1008번

--- A_B.py
+++ A_B.py
@@ -1,3 +1,17 @@
-A, B = map(int,input().split())
+'''A, B = map(int,input().split()) # 외울것
 
 print (A+B)
+'''
+
+'''A, B = map(int,input().split()) 
+
+print (A-B)'''
+
+
+'''A, B = map(int,input().split())
+
+print (A*B)'''
+
+A, B = map(int,input().split())
+
+print (f"{A/B}")
