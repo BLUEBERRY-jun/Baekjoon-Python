dc6d740129dd2334d69aba46f58b20ea8d6e992b
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-15 11:43:57 +0900

    25206

---
+++ 너의_평점은.py
@@ -0,0 +1,23 @@
+import sys
+
+grade_to_score = {
+    "A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0,
+    "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
+
+total_score = 0
+total_credit = 0
+
+for _ in range(20):
+    data = sys.stdin.readline().split()
+    if not data: break
+
+    credit = float(data[1])
+    grade = data[2]
+
+    if grade == "P":
+        continue
+
+    total_score += credit * grade_to_score[grade]
+    total_credit += credit
+
+print ("{:.6f}".format(total_score/total_credit))
