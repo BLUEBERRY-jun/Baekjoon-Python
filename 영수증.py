f2c0afe56700cb96fe66d54093f404a2f364a14a
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-17 17:33:50 +0900

    25304번

---
+++ 영수증.py
@@ -0,0 +1,15 @@
+import sys
+
+X = int(sys.stdin.readline())
+N = int(sys.stdin.readline())
+
+total_price=0
+
+for _ in range(N):
+    a, b = map(int,sys.stdin.readline().split())
+    total_price += a*b
+
+if X == total_price:
+    print ("Yes")
+else:
+    print ("No")
