1825db1825799b93c39d6a7d086d60fcaf060136
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-20 17:49:26 +0900

    10809번

---
+++ 5_문자열/알파벳_찾기.py
@@ -0,0 +1,15 @@
+# S = input()
+# alpabet = 'abcdefghijklmnopqrstuvwxyz'
+
+# for i in alpabet:
+#     print (S.find(i), end=" ")
+
+# S=input()
+# alpabet_ord = range(97, 123)
+
+# for i in alpabet_ord:
+#       char = chr(i)
+#       print (S.find(char), end=" ")  
+
+a = map(int, ["1", "2"])
+print(a)
