d61c11d5376edac8c5bdc316ec802125abd145ce
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-20 17:51:16 +0900

    10809번

--- 5_문자열/알파벳_찾기.py
+++ 5_문자열/알파벳_찾기.py
@@ -4,12 +4,11 @@
 # for i in alpabet:
 #     print (S.find(i), end=" ")
 
-# S=input()
-# alpabet_ord = range(97, 123)
+S=input()
+alpabet_ord = range(97, 123)
 
-# for i in alpabet_ord:
-#       char = chr(i)
-#       print (S.find(char), end=" ")  
+for i in alpabet_ord:
+      char = chr(i)
+      print (S.find(char), end=" ")  
 
-a = map(int, ["1", "2"])
-print(a)
+# map 기능 정확히 이해하
