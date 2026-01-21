76a1f03d0df01e5a9212cf68464b6c31e28156c1
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-21 17:11:16 +0900

    3003번

--- 6_심화1/킹_퀸_룩_비숍_나이트_폰.py
+++ 6_심화1/킹_퀸_룩_비숍_나이트_폰.py
@@ -1,5 +1,45 @@
-fix = list(map(int, input().split()))
-origin = [1, 1, 2, 2, 2, 8]
+# import sys
+# x = [1, 1, 2, 2, 2, 8]
+# y = list(map(int, input().split()))
 
-result = (a - b for a, b in zip(origin , fix))
+# result = []
+# for i in range(6):
+#     value = x[i]-y[i]
+#     result.append(value)
+# print (*result)
+
+
+# import sys
+# x = [1, 1, 2, 2, 2, 8]
+# y = list(map(int, input().split()))
+
+# result = [x[i]-y[i] for i in range(6)]
+# print (*result)
+
+
+# import sys
+# x = [1, 1, 2, 2, 2, 8]
+# y = list(map(int, input().split()))
+
+# result = []
+# for a, b in zip(x, y):
+#     result.append(a - b)
+
+# print(*result)
+
+
+# import sys
+# x = [1, 1, 2, 2, 2, 8]
+# y = list(map(int, input().split()))
+
+# result = [a - b for a, b in zip (x, y)]
+
+# print(*result)
+
+import sys
+x = [1, 1, 2, 2, 2, 8]
+y = list(map(int, input().split()))
+
+result = map(lambda a, b: a - b, x, y)
+
 print(*result)
