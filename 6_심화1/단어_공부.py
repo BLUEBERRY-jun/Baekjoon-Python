f72b080d81d7019befe111c49c1bb3cba84baf1d
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-26 13:10:25 +0900

    1157번

--- 6_심화1/단어_공부.py
+++ 6_심화1/단어_공부.py
@@ -1,19 +1,33 @@
+# x = input().upper()
+# y = list(set(x))
+# y.sort()
+
+# counts = []
+# for i in y:
+#     count = x.count(i)
+#     counts.append(count)
+
+# if counts.count(max(counts))>1:
+#     print("?")
+# else:
+#     max_val=max(counts) #count 안에서 가장큰값을 max_val안에 넣는다
+#     max_index=counts.index(max_val) #max_val값의 위치를 max_index에 넣는다
+#     print (y[max_index])
+
+#  ai도움으로 품 나중에 다시 풀 것 + 아스키 코드사용해서 풀어볼것
+
+
+
 x = input().upper()
 y = list(set(x))
-y.sort()
 
 counts = []
+max_count = 0
 for i in y:
-    count = x.count(i)
-    counts.append(count)
+    counts.append(x.count(i)) # y 개수 센 리스트 생성
 
-
+    
 if counts.count(max(counts))>1:
-    print("?")
+    print ("?")
 else:
-    max_val=max(counts) #count 안에서 가장큰값을 max_val안에 넣는다
-    max_index=counts.index(max_val) #max_val값의 위치를 max_index에 넣는다
-    print (y[max_index])
-
-# ai도움으로 품 나중에 다시 풀 것 + 아스키 코드사용해서 풀어볼것 
-
+    print (y[counts.index(max(counts))])
