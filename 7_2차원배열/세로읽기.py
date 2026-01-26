a9022026c49f1554dc7b7b3551cb09fedaa982e9
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-26 16:06:17 +0900

    10798번

--- 7_2차원배열/세로읽기.py
+++ 7_2차원배열/세로읽기.py
@@ -1,11 +1,23 @@
+# import sys
+
+# words = []
+# for _ in range(5):
+#     line = sys.stdin.readline().strip()
+#     words.append(line)
+
+# for j in range(15):
+#     for i in range(5):
+#         if j < len(words[i]):
+#             print(words [i][j], end="")
+
+
 import sys
 
 words = []
 for _ in range(5):
-    line = sys.stdin.readline().strip()
-    words.append(line)
-
+    words.append (sys.stdin.readline().strip())
+    
 for j in range(15):
     for i in range(5):
         if j < len(words[i]):
-            print(words[i][j], end="")
+            print(words [i][j], end="")  # 2번째 내일 다시
