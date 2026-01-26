2ee4d6a72de6e84fd54e6ae5504c6b0e37565125
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-26 13:16:40 +0900

    2941번

--- 6_심화1/크로아티아_알파벳.py
+++ 6_심화1/크로아티아_알파벳.py
@@ -1,9 +1,16 @@
-cro_list="c=,c-,dz=,d-,lj,nj,s=,z=".split(',')
-cro = input()
+# cro_list="c=,c-,dz=,d-,lj,nj,s=,z=".split(',')
+# cro = input()
+
+# for i in (cro_list):
+#     cro = cro.replace(i, '*') #크로아티아 알파벳을 * 로 바꾼다 
+    
+# print(len(cro))
 
 
-for i in (cro_list):
-    cro = cro.replace(i, '*')
+x = input()
+y = ["c=", "c-","dz=","d-","lj", "nj", "s=", "z="]
 
+for i in y:
+    x = x.replace(i,"*")
 
-print(len(cro))
+print(len(x))
