9a5a4daf302903e1dc7d92439fee04c797c302c8
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-26 16:06:01 +0900

    2566번

--- 7_2차원배열/최댓값.py
+++ 7_2차원배열/최댓값.py
@@ -1,9 +1,23 @@
 # import sys
 
-# num = list(map(int, sys.stdin.read().split()))
+# M = []
+# for i in range(9):
+#     M.append(list(map(int, sys.stdin.readline().split())))
 
-# print(max(num))
-# print(num.index(max(num))+1)
+# max_val = -1
+# max_row = 0
+# max_col = 0
+
+# for i in range(9):
+#     for j in range(9):
+#         if M[i][j] > max_val:
+#             max_val=M[i][j]
+#             max_row=i+1
+#             max_col=j+1
+
+# print(max_val)
+# print(max_row, max_col)
+
 
 import sys
 
@@ -11,16 +25,16 @@ M = []
 for i in range(9):
     M.append(list(map(int, sys.stdin.readline().split())))
 
-max_val = 0
-max_row = 0
-max_col = 0
+Max_value = -1
+Max_x = 0
+Max_y = 0
 
 for i in range(9):
     for j in range(9):
-        if M[i][j] > max_val:
-            max_val=M[i][j]
-            max_row=i+1
-            max_col=j+1
+        if M[i][j] > Max_value:
+            Max_value = M[i][j]
+            Max_x = i+1
+            Max_y = j+1
 
-print(max_val)
-print(max_row, max_col)
+print (Max_value)
+print (Max_x, Max_y)  # 2번째 내일 다시
