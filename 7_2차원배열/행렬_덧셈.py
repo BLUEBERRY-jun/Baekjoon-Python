f67ef960ccf7c1523a74555f0da69a40bcba77cf
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-26 16:05:43 +0900

    2738번번

--- 7_2차원배열/행렬_덧셈.py
+++ 7_2차원배열/행렬_덧셈.py
@@ -1,18 +1,36 @@
+# import sys
+
+# N, M = map(int, sys.stdin.readline().split())
+
+# matrix_A=[]
+# for i in range(N):
+#     matrix_A.append(list(map(int, sys.stdin.readline().split())))
+
+# matrix_B=[]
+# for i in range(N):
+#     matrix_B.append(list(map(int, sys.stdin.readline().split())))
+
+# for i in range(N):
+#     for j in range(M):
+#         print(matrix_A[i][j]+matrix_B[i][j],end=" ")
+#     print()
+
+    #ai로 공부
+
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-matrix_A=[]
-for i in range(N):
-    matrix_A.append(list(map(int, sys.stdin.readline().split())))
+matrix_x = []
+matrix_y = []
 
-matrix_B=[]
-for i in range(N):
-    matrix_B.append(list(map(int, sys.stdin.readline().split())))
+for i in range (N):
+    matrix_x.append(list(map(int, sys.stdin.readline().split())))
+
+for j in range (N):
+    matrix_y.append(list(map(int, sys.stdin.readline().split())))
 
 for i in range(N):
     for j in range(M):
-        print(matrix_A[i][j]+matrix_B[i][j],end=" ")
-    print()
-
-    #ai로 공부
+        print(matrix_x[i][j]+matrix_y[i][j],end=" ")
+    print () # 2번째 내일 다시
