a0fce2f78ccf65f1f60efee55b9dd028f0932075
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-27 20:15:21 +0900

    2745번

--- 8_수학1/진법_변환.py
+++ 8_수학1/진법_변환.py
@@ -3,16 +3,44 @@
 # N, B = sys.stdin.readline().split()
 # print(int(N, int(B)))
 
+# import sys
+# N, B = sys.stdin.readline().split()
+# B = int(B)
+# table = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# ans = 0
+# N = N[::-1]
+
+# for i in range(len(N)):
+#     val = table.index(N[i])
+#     ans += val * (B ** i )
+
+# print (ans)
+
+
+
+
+# import sys
+
+# N, B = sys.stdin.readline().split()
+# B = int(B)
+
+# print (int(N, B))
+
+
+
 import sys
+
 N, B = sys.stdin.readline().split()
 B = int(B)
 table = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-ans = 0
 N = N[::-1]
+x = 0
+result = 0
 
 for i in range(len(N)):
-    val = table.index(N[i])
-    ans += val * (B ** i )
+    x = table.index(N[i])
+    result += x * (B ** i)  #B에 진법 
 
-print (ans)
+print (result)  #2번쨰 
