c9f580c58c3f1bf4e7bea67222c399469c7c2e7f
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-27 19:40:37 +0900

    2738번

--- 7_2차원배열/행렬_덧셈.py
+++ 7_2차원배열/행렬_덧셈.py
@@ -17,20 +17,41 @@
 
     #ai로 공부
 
+# import sys
+
+# N, M = map(int, sys.stdin.readline().split())
+
+# matrix_x = []
+# matrix_y = []
+
+# for i in range (N):
+#     matrix_x.append(list(map(int, sys.stdin.readline().split())))
+
+# for j in range (N):
+#     matrix_y.append(list(map(int, sys.stdin.readline().split())))
+
+# for i in range(N):
+#     for j in range(M):
+#         print(matrix_x[i][j]+matrix_y[i][j],end=" ")
+#     print () # 2번째 내일 다시
+
+
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-matrix_x = []
-matrix_y = []
+list_x = []
+list_y = []
 
-for i in range (N):
-    matrix_x.append(list(map(int, sys.stdin.readline().split())))
 
-for j in range (N):
-    matrix_y.append(list(map(int, sys.stdin.readline().split())))
+for i in range(N):
+    list_x.append(list(map(int, sys.stdin.readline().split())))
+
+for j in range(N):
+    list_y.append(list(map(int, sys.stdin.readline().split())))
 
 for i in range(N):
     for j in range(M):
-        print(matrix_x[i][j]+matrix_y[i][j],end=" ")
-    print () # 2번째 내일 다시
+        print (list_x[i][j] + list_y[i][j], end=" ")
+    print () #3번째
+
