a67abe24661b660e98626399563914212dd56994
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-28 11:19:16 +0900

    2903번

---
+++ 8_수학1/중앙_이동_알고리즘.py
@@ -0,0 +1,12 @@
+# N = int(input())
+
+# result = 0
+# x=2
+# for _ in range(N):
+#     result = (x+(x-1)) * (x+(x-1))
+#     x = x+(x-1)
+
+# print(result)
+
+N = int(input())
+print((2**N + 1) ** 2)
