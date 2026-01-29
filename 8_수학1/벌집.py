957a199019309318f8412d010b0303913f209986
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-29 13:59:30 +0900

    2292번

--- 8_수학1/벌집.py
+++ 8_수학1/벌집.py
@@ -1,26 +1,32 @@
 # N = int(input())
-
 # x=1
 # y=1
-
-# while N > x:
-#     x += 6 * y
+# i=0
+# while True:
+#     if x >= N:
+#         break
+#     i += 1
 #     y += 1
+#     x += 6*i
 
+    
 # print (y)
 
 
 
-N = int(input())
-x=1
-y=1
-i=0
-while True:
-    if x > N:
-        break
-    i += 1
-    y += 1
-    x += 6*i
 
-    
-print (y)
+N = int(input())
+
+line = 1
+x = 1
+i = 0
+
+while True:
+    if x >= N:
+        break
+    else:
+        line += 1
+        i += 1
+        x += 6*i
+
+print (line)
