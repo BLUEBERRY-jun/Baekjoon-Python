137d36ca887734aca2601273f823875a663a173c
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-29 13:41:11 +0900

    11005번

--- 8_수학1/진법_변환_2.py
+++ 8_수학1/진법_변환_2.py
@@ -12,17 +12,36 @@
 # print (result[::-1])
 
 
+# import sys
+
+# N, B = map(int, sys.stdin.readline().split())
+# table = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# x = 0
+# result = ""
+# while N > 0:
+#     x = N % B 
+#     N = N // B
+#     result += table[x]  #string은 append 불까
+
+# print(result[::-1])   #처음 몫이 1의자리 수이기 떄문에 뒤집어줘야함
+#2번째
+
+
+
+
 import sys
 
 N, B = map(int, sys.stdin.readline().split())
+B = int(B)
 table = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 x = 0
 result = ""
-while N > 0:
-    x = N % B 
-    N = N // B
-    result += table[x]  #string은 append 불까
 
-print(result[::-1])   #처음 몫이 1의자리 수이기 떄문에 뒤집어줘야함
-#2번째
+while N > 0:
+    x = N % B
+    N = N // B
+    result += table[x]
+
+print (result [::-1])   #3번째
