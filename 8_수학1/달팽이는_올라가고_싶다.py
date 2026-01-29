e1833cff9fa7b973dc62119a45d30509e0165715
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-29 14:20:48 +0900

    2869번

--- 8_수학1/달팽이는_올라가고_싶다.py
+++ 8_수학1/달팽이는_올라가고_싶다.py
@@ -13,10 +13,24 @@
 
 # print(day)    #수가 많으면 오류남
 
+# import sys
+
+# A, B, V = map(int,sys.stdin.readline().split())
+
+# day = (V-B-1)//(A-B) + 1    #올림 매서드 안쓰고 수식으로 해결하는 법
+
+# print(day)
+
+
+
+
+
+
 import sys
 
-A, B, V = map(int,sys.stdin.readline().split())
+A, B ,V = map(int, sys.stdin.readline().split())
 
-day = (V-B-1)//(A-B) + 1
+x = V - A #올라갈양
+A - B #올라갔다 잔거임 이건
 
-print(day)
+print ((V-B-1)//(A-B)+1)
