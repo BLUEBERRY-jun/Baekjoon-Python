8dfd99837a4ae4312d6aa3cc549e3671e16dd1b5
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-31 17:48:04 +0900

    10871번

--- 4_1차원배열/X보다_작은_수.py
+++ 4_1차원배열/X보다_작은_수.py
@@ -1,8 +1,28 @@
+# import sys
+
+# N, X = map(int, sys.stdin.readline().split())
+# num = list(map(int, sys.stdin.readline().split()))
+
+# for i in num:
+#     if i < X:
+#         print (i, end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
 import sys
-
 N, X = map(int, sys.stdin.readline().split())
-num = list(map(int, sys.stdin.readline().split()))
+N_list = list(map(int, sys.stdin.readline().split()))
 
-for i in num:
+for i in N_list:
     if i < X:
-        print (i, end=" ")
+        print(i, end=" ")
