f47e303121d1f3f637b6a5871c74e026fe78d648
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-31 18:36:54 +0900

    3052번

--- 4_1차원배열/나머지.py
+++ 4_1차원배열/나머지.py
@@ -1,9 +1,21 @@
+# import sys
+
+# num = list(map(int, sys.stdin.read().split()))
+# result = []
+
+# for i in num:
+#     result.append(i % 42)
+
+# print (len(set(result)))
+
+
+
+
 import sys
+result_list = set()
+x = list(map(int, sys.stdin.read().split()))
 
-num = list(map(int, sys.stdin.read().split()))
-result = []
+for i in range(10):
+    result_list.add(x[i]%42)
 
-for i in num:
-    result.append(i % 42)
-
-print (len(set(result)))
+print(len(result_list))
