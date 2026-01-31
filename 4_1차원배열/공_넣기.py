700a1ad4feef5d00e3a4149c44664e3796ec2eaf
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-31 18:09:27 +0900

    10810번

--- 4_1차원배열/공_넣기.py
+++ 4_1차원배열/공_넣기.py
@@ -1,10 +1,25 @@
+# import sys
+
+# N, M = map(int, sys.stdin.readline().split())
+# baskets = [0] * (N)
+
+# for _ in sys.stdin:
+#     i, j, k = map(int,_.split())
+#     baskets[i-1:j] = [k] * (j-i+1)
+
+# print(*baskets)
+
+
+
+
+
 import sys
+N, M = map(int,sys.stdin.readline().split())
+baskets = [0] * N
 
-N, M = map(int, sys.stdin.readline().split())
-baskets = [0] * (N)
+for _ in range(M):
+    i, j, k = map(int, sys.stdin.readline().split())
+    baskets[i-1:j] = [k] * (j-(i-1))    
+    #앞에 수만큼 뒤에 넣어줘야함 넣을 때 []쓸 것 
 
-for _ in sys.stdin:
-    i, j, k = map(int,_.split())
-    baskets[i-1:j] = [k] * (j-i+1)
-
-print(*baskets)
+print (*baskets)
