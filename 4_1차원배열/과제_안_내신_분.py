484c45f6496927dae1b7c6f3ad3d1f7133fe6aab
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-31 18:28:22 +0900

    5597번

--- 4_1차원배열/과제_안_내신_분.py
+++ 4_1차원배열/과제_안_내신_분.py
@@ -1,11 +1,24 @@
+# import sys
+
+# stu = list(range(31))
+# sukjae=list(map(int, sys.stdin.read().split()))
+
+# for i in range(len(sukjae)):
+#     stu.remove(sukjae[i]) #pop remove 차이
+
+# for j in stu:
+#     if j != 0:
+#         print(j)
+
+
+
+
+#set으로 차집합, for zip, remove로 가능함
 import sys
+x_list = range(1,30)
+y_list = list(map(int,sys.stdin.read().split()))
+result_list = list(set(x_list)-set(y_list))
+result_list.sort()
 
-stu = list(range(31))
-sukjae=list(map(int, sys.stdin.read().split()))
-
-for i in range(len(sukjae)):
-    stu.remove(sukjae[i]) #pop remove 차이
-
-for j in stu:
-    if j != 0:
-        print(j)
+for i in result_list:
+    print(i)
