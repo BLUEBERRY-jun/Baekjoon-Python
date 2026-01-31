8c4de8d03ca5f7183b03af2458c1bbb95f572fe2
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-31 17:44:44 +0900

    10807번

--- 4_1차원배열/개수_세기.py
+++ 4_1차원배열/개수_세기.py
@@ -1,5 +1,14 @@
-N =  int(input())
-num = list(map(int, input().split()))
-v = int(input())
+# N =  int(input())
+# num = list(map(int, input().split()))
+# v = int(input())
 
-print (num.count(v)).
+# print (num.count(v))
+
+
+
+import sys
+N = int(sys.stdin.readline())
+N_list = list(map(int, sys.stdin.readline().split()))
+v = int(sys.stdin.readline())
+
+print (N_list.count(v)) #2회차
