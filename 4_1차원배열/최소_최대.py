1055288b1599d96eba1aebb99c2adb964c820556
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-01-31 17:50:46 +0900

    10818번

--- 4_1차원배열/최소_최대.py
+++ 4_1차원배열/최소_최대.py
@@ -1,6 +1,15 @@
+# import sys
+
+# N = int(input())
+# num = list(map(int, sys.stdin.readline().split()))
+
+# print (min(num), max(num))
+
+
+
+
 import sys
+N = int(sys.stdin.readline())
+N_list = list(map(int, sys.stdin.readline().split()))
 
-N = int(input())
-num = list(map(int, sys.stdin.readline().split()))
-
-print (min(num), max(num))
+print (min(N_list), max(N_list))    #2회차
