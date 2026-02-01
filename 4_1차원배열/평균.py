76ac264d0d54e98036a68a51de6104fd458accde
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-01 20:26:44 +0900

    1546번

--- 4_1차원배열/평균.py
+++ 4_1차원배열/평균.py
@@ -1,11 +1,22 @@
+# import sys
+
+# N = int(input())
+# score = list(map(int, sys.stdin.readline().split()))
+
+# M = max(score)
+
+# for i in range(N):
+#     score[i] = score[i]/M*100
+
+# print (sum(score)/N)
+
+
+
+
 import sys
 
-N = int(input())
-score = list(map(int, sys.stdin.readline().split()))
+N= int(sys.stdin.readline())
+N_list = list(map(int,sys.stdin.readline().split()))
+M = max(N_list)
 
-M = max(score)
-
-for i in range(N):
-    score[i] = score[i]/M*100
-
-print (sum(score)/N)
+print(sum(N_list)*100/M/N)
