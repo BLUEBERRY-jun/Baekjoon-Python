08ee44544b43cf1d00de4a6323c898d7d72bcaa7
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-01 20:19:33 +0900

    10811번

--- 4_1차원배열/바구니_뒤집기.py
+++ 4_1차원배열/바구니_뒤집기.py
@@ -1,10 +1,24 @@
+# import sys
+
+# N,M = map(int, sys.stdin.readline().split())
+# baskets = list(range(N+1))
+
+# for _ in range(M):
+#     i, j = map(int, sys.stdin.readline().split())
+#     baskets[i:j+1] = baskets[i:j+1][::-1] #[이상:미만]
+
+# print(*baskets[1:])
+
+
+
+
 import sys
 
-N,M = map(int, sys.stdin.readline().split())
-baskets = list(range(N+1))
+N, M = map(int,sys.stdin.readline().split())
+N_list = list(range(N+1))
 
 for _ in range(M):
-    i, j = map(int, sys.stdin.readline().split())
-    baskets[i:j+1] = baskets[i:j+1][::-1]
+    x, y = map(int,sys.stdin.readline().split())
+    N_list[x:y+1] = N_list[x:y+1][::-1]
 
-print(*baskets[1:])
+print(*N_list[1:])
