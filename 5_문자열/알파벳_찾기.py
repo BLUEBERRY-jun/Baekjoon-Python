ceda222445f8fa2067c898ef251ef011dfd99d44
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-02 14:01:14 +0900

    10809번

--- 5_문자열/알파벳_찾기.py
+++ 5_문자열/알파벳_찾기.py
@@ -4,9 +4,21 @@
 # for i in alpabet:
 #     print (S.find(i), end=" ")
 
-S=input()
-alpabet_ord = range(97, 123)
+# S=input()
+# alpabet_ord = range(97, 123)
 
-for i in alpabet_ord:
-      char = chr(i)
-      print (S.find(char), end=" ")  
+# for i in alpabet_ord:
+#       char = chr(i)
+#       print (S.find(char), end=" ")  
+
+
+
+
+
+import sys
+s = sys.stdin.readline().strip()
+s_list = range(97, 123)
+
+for i in s_list:
+    i = chr(i)    #아스키코드를 알파벳으로 변환
+    print (s.find(i), end=" ")      #count는 없을때 0 find는 -1
