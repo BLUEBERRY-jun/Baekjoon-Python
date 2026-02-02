e425714bf5d5614ced59bdc254a9175ac451b0bb
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-02 14:11:07 +0900

    2908번

--- 5_문자열/상수.py
+++ 5_문자열/상수.py
@@ -1,12 +1,27 @@
-import sys
+# import sys
 
-x, y = sys.stdin.readline().split()
-x = int(x[::-1])
-y = int(y[::-1])
+# x, y = sys.stdin.readline().split()
+# x = int(x[::-1])
+# y = int(y[::-1])
 
 # if x-y>0:
 #     print(x)
 # elif x-y<0:
 #     print(y)
 
-print(max(x, y))
+# print(max(x, y))
+
+
+
+
+
+import sys
+a, b = map(int,sys.stdin.readline().split())
+
+a = int(str(a)[::-1])
+b = int(str(b)[::-1])
+
+if a > b:
+    print(a)
+else:
+    print(b)
