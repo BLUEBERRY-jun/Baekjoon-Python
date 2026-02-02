89caa4657797249047488926996876d583f5058e
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-02 13:53:01 +0900

    11720번

--- 5_문자열/숫자의_합.py
+++ 5_문자열/숫자의_합.py
@@ -1,13 +1,27 @@
-# N=int(input())
+# # N=int(input())
+# # M=input()
+# # total_M = 0
+
+# # for i in range(N):
+# #     total_M += int(M[i])
+
+# # print (total_M)
+
+# N=input()
 # M=input()
-# total_M = 0
 
-# for i in range(N):
-#     total_M += int(M[i])
+# print(sum(map(int, M))) #map이 한개씩 들고온다라고 이해
 
-# print (total_M)
 
-N=input()
-M=input()
 
-print(sum(map(int, M)))
+
+
+import sys
+N = int(sys.stdin.readline().strip())
+N_list = list(map(int, sys.stdin.readline().strip()))
+result = 0
+
+for i in N_list:
+    result += i
+
+print(result)
