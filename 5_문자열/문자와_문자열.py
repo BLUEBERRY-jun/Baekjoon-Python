e1a5feda31f35e32e54f9e5085ff1ae945811770
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-02 13:42:27 +0900

    27866번

--- 5_문자열/문자와_문자열.py
+++ 5_문자열/문자와_문자열.py
@@ -1,5 +1,14 @@
-import sys
-S, i = sys.stdin.read().split()
+# import sys
+# S, i = sys.stdin.read().split()
 
-i=int(i)
-print(S[i-1])
+# i=int(i)
+# print(S[i-1])
+
+
+
+
+import sys
+s = sys.stdin.readline()
+i = int(sys.stdin.readline())
+
+print (s[i-1])
