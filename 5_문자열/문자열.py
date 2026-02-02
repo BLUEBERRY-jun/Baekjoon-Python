65ef2b20f8e8fbc693491f836ac4571698eb9bf7
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-02 14:29:29 +0900

    9086번

--- 5_문자열/문자열.py
+++ 5_문자열/문자열.py
@@ -1,7 +1,19 @@
-import sys
+# import sys
 
+# T = int(sys.stdin.readline())
+
+# for _ in range(T):
+#     x = sys.stdin.readline().strip()
+#     print(x[0] + x[-1])
+
+
+
+
+import sys
 T = int(sys.stdin.readline())
+T_list = []
 
 for _ in range(T):
-    x = sys.stdin.readline().strip()
-    print(x[0] + x[-1])
+    T_list = list(sys.stdin.readline().strip()) 
+    #\n까지 입력받아서 strip으로 삭제해야함
+    print (T_list[0]+T_list[-1])
