e40ab2c8ceb7f002f25feedf96b18c9cb8c29d27
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-02 14:05:39 +0900

    2675번

--- 5_문자열/문자열_반복.py
+++ 5_문자열/문자열_반복.py
@@ -1,12 +1,26 @@
+# import sys
+
+
+# T= int(sys.stdin.readline())
+# for _ in range(T):
+#     result=""           #""chr형 0int형
+#     R, S = sys.stdin.readline().split()
+#     R = int (R)
+#     for i in S:
+#         result += i*R
+
+#     print(result)
+
+
+
+
+
 import sys
+s = int(sys.stdin.readline())
 
-
-T= int(sys.stdin.readline())
-for _ in range(T):
-    result=""
-    R, S = sys.stdin.readline().split()
-    R = int (R)
-    for i in S:
-        result += i*R
-
-    print(result)
+for _ in range(s):
+    r, p = sys.stdin.readline().split()
+    r = int(r)
+    for i in p:
+        print(i*r,end="")
+    print()
