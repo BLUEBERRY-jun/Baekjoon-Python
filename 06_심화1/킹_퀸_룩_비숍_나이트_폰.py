05e9842a6cc0d0f74a41ff011278efdff05cb95b
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-07 21:15:04 +0900

    3003번

--- 06_심화1/킹_퀸_룩_비숍_나이트_폰.py
+++ 06_심화1/킹_퀸_룩_비숍_나이트_폰.py
@@ -47,13 +47,27 @@
 
 
 
+# import sys
+
+# x = [1, 1, 2, 2, 2, 8]
+# y =  list(map(int, sys.stdin.readline().split()))
+# chess_result = []
+
+# for a, b in zip(x,y):
+#     chess_result.append(a-b)
+
+# print (*chess_result) #zip 공식 외울것 그럼 쉬움
+
+
+
+
+
 import sys
+ans = [1, 1, 2, 2, 2, 8]
+mine = list(map(int, sys.stdin.readline().split()))
 
-x = [1, 1, 2, 2, 2, 8]
-y =  list(map(int, sys.stdin.readline().split()))
-chess_result = []
+result = []
+for a, b in zip(ans, mine):
+    result.append(a-b)
 
-for a, b in zip(x,y):
-    chess_result.append(a-b)
-
-print (*chess_result) #zip 공식 외울것 그럼 쉬움
+print(*result)
