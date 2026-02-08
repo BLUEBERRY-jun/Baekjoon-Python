3ad495db84af3bd0b71f80a2881f95e4baf100c5
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-08 19:29:32 +0900

    25206번

--- 06_심화1/너의_평점은.py
+++ 06_심화1/너의_평점은.py
@@ -24,24 +24,50 @@
 
 
 
+# import sys
+
+# grade_to_score = {
+#      "A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0,
+#      "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0} #{}딕셔너리
+
+# total_score = 0
+# total_credit = 0
+
+# for i in range(20):
+#     data = sys.stdin.readline().split()
+
+#     credit = float(data[1]) #실수형
+#     grade = data[2]
+
+#     if grade == "P":
+#         continue
+    
+#     total_score += credit * grade_to_score[grade]
+#     total_credit += credit
+
+# print ("{:.6f}".format(total_score/total_credit)) #format 쓰는법
+
+
+
+
+
 import sys
 
 grade_to_score = {
      "A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0,
-     "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0} #{}딕셔너리
+     "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
 
-total_score = 0
-total_credit = 0
+data = []
+credit_s = 0
+result = 0
 
-for i in range(20):
+for _ in range(20):
     data = sys.stdin.readline().split()
-    credit = float(data[1]) 
+    credit = float(data[1])
     grade = data[2]
-
     if grade == "P":
         continue
-    
-    total_score += credit * grade_to_score[grade]
-    total_credit += credit
+    result += credit * grade_to_score[grade]
+    credit_s += credit
 
-print ("{:.6f}".format(total_score/total_credit))
+print("{:.6f}".format(result/credit_s))
