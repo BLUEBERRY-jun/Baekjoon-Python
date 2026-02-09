f7ba4c9dd1105199351adc5431a7caa6088cdba5
BLUEBERRY-jun <dlatmdwnsdla@gmail.com>
2026-02-09 21:43:42 +0900

    2563번

--- 07_2차원배열/색종이.py
+++ 07_2차원배열/색종이.py
@@ -41,23 +41,46 @@
 # print(total)  # 2번째 내일 다시
 
 
+# import sys
+
+# N = int(sys.stdin.readline())
+
+# paper = []
+# for _ in range (100):
+#     paper.append([0]*100)
+
+# for _ in range(N):
+#     x, y = map(int, sys.stdin.readline().split())
+#     for i in range(x, x+10):
+#         for j in range(y, y+10):
+#             paper[i][j] = 1
+
+# total = 0
+
+# for i in range(100):
+#     total += paper[i].count(1)
+
+# print(total)    #3번째
+
+
+
+
+
 import sys
 
 N = int(sys.stdin.readline())
 
 paper = []
-for _ in range (100):
+for _ in range(100):
     paper.append([0]*100)
 
 for _ in range(N):
-    x, y = map(int, sys.stdin.readline().split())
+    x, y =map(int,sys.stdin.readline().split())
     for i in range(x, x+10):
         for j in range(y, y+10):
             paper[i][j] = 1
 
 total = 0
-
-for i in range(100):
-    total += paper[i].count(1)
-
-print(total)    #3번째
+for z in paper:
+    total += z.count(1)
+print(total)
